c70fc7673bf16436117e05c21b7d406221399679
Suhasshende23 <suhas.shende@vervelo.com>
2025-09-25 16:27:33 +0530

    Questions and Choice Model

--- django_projects/mysite/polls/models.py
+++ django_projects/mysite/polls/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Questions(models.Model):
+    text=models.CharField(max_length=50,null=True,blank=True)
+    pub_date = models.DateTimeField("date published")
+    def __str__(self):
+        return f"{self.text}"
+
+class Choice(models.Model):
+    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self): 
+        return f"{self.question.text} ->{self.choice_text}"
