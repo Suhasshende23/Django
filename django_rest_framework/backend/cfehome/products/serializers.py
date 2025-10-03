0dc3150cbfd9b78d983fb6dec70ab9f8fb786409
Suhasshende23 <suhas.shende@vervelo.com>
2025-10-03 19:05:31 +0530

    urls reverse and serializers

--- django_rest_framework/backend/cfehome/products/serializers.py
+++ django_rest_framework/backend/cfehome/products/serializers.py
@@ -6,9 +6,15 @@ from .models import Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model=Product
-        fields=["pk","title","content","price","sale_price","get_discount"]
+        fields=["pk","url","title","content","price","sale_price","get_discount"]
 
-    
+        url = serializers.HyperlinkedIdentityField(
+            view_name='product-detail',
+            lookup_field='pk',
+            read_only=True
+              )
+
+
     def get_my_discount(self, obj):
         if not hasattr(obj, 'id'):
             return None
